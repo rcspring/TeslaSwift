cbebd3f66ab953c6ee0f93147f1647f25f4c7ded
Ryan Spring <ryan@extramutt.com>
2021-06-21 09:42:07 -0400

    Hacked for iOS 15 support.

---
+++ Sources/TeslaSwift/Stuff.swift
@@ -0,0 +1,16 @@
+//
+//  Stuff.swift
+//  TeslaSwift
+//
+//  Created by Ryan Spring on 6/18/21.
+//  Copyright © 2021 Joao Nunes. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+//struct TeslaLoginView: UIViewRepresentable {    
+//    typealias UIViewType = TeslaWebLoginViewController
+//    
+//    
+//}
