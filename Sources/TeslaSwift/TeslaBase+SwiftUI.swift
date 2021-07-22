61fc9748dae76d9e0a0160b670e6be1e18ceea34
Ryan Spring <ryan@extramutt.com>
2021-07-22 07:35:21 -0700

    Added Environment Values.

---
+++ Sources/TeslaSwift/TeslaBase+SwiftUI.swift
@@ -0,0 +1,21 @@
+//
+//  TeslaBase+SwiftUI.swift
+//  TeslaBase+SwiftUI
+//
+//  Created by Ryan Spring on 7/22/21.
+//  Copyright © 2021 Joao Nunes. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct TeslaBaseKey: EnvironmentKey {
+    static let defaultValue = TeslaBase.shared
+}
+
+extension EnvironmentValues {
+    var teslaBase: TeslaBase {
+        get { self[TeslaBaseKey.self] }
+        set { self[TeslaBaseKey.self] = newValue }
+    }
+}
